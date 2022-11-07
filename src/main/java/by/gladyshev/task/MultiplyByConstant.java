2751a447dea6b0b7404bfb065ab7d890e81475cb
Goldtool-c <75798485+Goldtool-c@users.noreply.github.com>
2022-11-07 14:51:37 +0300

    3.0

--- src/main/java/by/gladyshev/task/MultiplyByConstant.java
+++ src/main/java/by/gladyshev/task/MultiplyByConstant.java
@@ -25,6 +25,9 @@ public class MultiplyByConstant {
                 if(blue > 255) {blue = 255;}
                 if(green > 255) {green = 255;}
                 if(red > 255) {red = 255;}
+                if(blue < 0) {blue = 0;}
+                if(green < 0) {green = 0;}
+                if(red < 0) {red = 0;}
                 result[i][j] = new Color(red, green, blue);
             }
         }
